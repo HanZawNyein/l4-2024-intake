68632cd2ef344f31afb4d5a14e6574ec826ff7f1
hanzawnyein <hanzawnyineonline@gmail.com>
2024-11-09 10:35:57 +0630

    [add] Scanner with next usage

--- Pop-PrinciplesOfProgramming/pop_java/source_code/basic_lessons/src/control_flow/lesson6.java
+++ Pop-PrinciplesOfProgramming/pop_java/source_code/basic_lessons/src/control_flow/lesson6.java
@@ -17,3 +17,15 @@ public class lesson6 {
 //object creation
 //class_name bj_name=new constructor();
 //Scanner sc = new Scanner(System.in);
+
+/*
+nextInt()	    Int	It takes int type input value from the user.
+nextFloat()	    Float	It takes a float type input value from the user.
+nextBoolean()	Boolean	It takes a boolean type input value from the user.
+nextLine()	    String	It takes a line as an input value from the user.
+next()	        String	It takes a word as an input value from the user.
+nextByte()	    Byte	It takes a byte type of input value from the user.
+nextDouble()	Double	It takes a double type input value from the user.
+nextShort()	    Short	It takes a short type input value from the user.
+nextLong()	    Long	It takes a long type of input value from the user.
+*/
