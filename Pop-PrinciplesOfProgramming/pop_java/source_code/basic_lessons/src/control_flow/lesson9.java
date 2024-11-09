3a1cd86946e9fd7b6135f6083102b1b37cc521b9
hanzawnyein <hanzawnyineonline@gmail.com>
2024-11-09 11:23:45 +0630

    [Lesson] switch case

---
+++ Pop-PrinciplesOfProgramming/pop_java/source_code/basic_lessons/src/control_flow/lesson9.java
@@ -0,0 +1,14 @@
+package control_flow;
+
+public class lesson9 {
+    public static void main(String[] args) {
+//        switch case
+        int i = 2;
+        switch (i) {
+            case 1:System.out.println("One");break;
+            case 2:System.out.println("two");break;
+            case 3:System.out.println("three");break;
+            default:System.out.println("Invalid");break;
+        }
+    }
+}
