055e866baca0bcead863bc2b7444d8c91106222e
hanzawnyein <hanzawnyineonline@gmail.com>
2024-11-16 11:13:46 +0630

    [Exercise] prime number

--- Pop-PrinciplesOfProgramming/pop_java/source_code/basic_lessons/src/control_flow/Exercise3.java
+++ Pop-PrinciplesOfProgramming/pop_java/source_code/basic_lessons/src/control_flow/Exercise3.java
@@ -2,17 +2,18 @@ package control_flow;
 
 public class Exercise3 {
     public static void main(String[] args) {
-        for (int row = 1; row <= 5; row++) {
-            for (int col = 1; col <= 5; col++) {
-                System.out.print("*"); // Print stars on the same line
+        boolean control = true;
+        for (int i = 3; i <= 100; i++) {
+            control = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    control=false;
+                    break;
+                }
             }
-            System.out.println(); // Move to the next line after each row
+            if(control){
+                System.out.println(i+" is prime number.");
+            }
         }
     }
 }
-//
-//*
-//**
-//***
-//****
-//*****
--- Pop-PrinciplesOfProgramming/pop_java/source_code/basic_lessons/src/control_flow/Exercise5.java
+++ Pop-PrinciplesOfProgramming/pop_java/source_code/basic_lessons/src/control_flow/Exercise5.java
@@ -1,18 +1,12 @@
 package control_flow;
 
-public class Exercise4 {
+public class Exercise5 {
     public static void main(String[] args) {
         for (int row = 1; row <= 5; row++) {
-            for (int col = 1; col <= 5; col++) {
+            for (int col = 1; col <= row; col++) {
                 System.out.print("*"); // Print stars on the same line
             }
             System.out.println(); // Move to the next line after each row
         }
     }
 }
-//
-//*
-//**
-//***
-//****
-//*****
