d7b8688bc6e6e8aba993e657dcc54770b10cdcf0
hanzawnyein <hanzawnyineonline@gmail.com>
2024-11-16 14:00:33 +0630

    [lesson] do while loop

--- Pop-PrinciplesOfProgramming/pop_java/source_code/basic_lessons/src/control_flow/Exercise7.java
+++ Pop-PrinciplesOfProgramming/pop_java/source_code/basic_lessons/src/control_flow/Exercise7.java
@@ -1,7 +1,27 @@
 package control_flow;
 
+import java.util.Scanner;
+
 public class Exercise7 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        String userChoice;
 
+        do {
+            System.out.println("Enter a number: ");
+            number = scanner.nextInt();
+
+            if (number % 2 == 0) {
+                System.out.println("Yes");
+            }
+
+            // Ask if the user wants to continue
+            System.out.println("Do you want to continue? (yes/no): ");
+            userChoice = scanner.next();
+
+        } while (userChoice.equalsIgnoreCase("yes")); // Continue if the user says "yes"
+
+        System.out.println("Program exited.");
     }
 }
