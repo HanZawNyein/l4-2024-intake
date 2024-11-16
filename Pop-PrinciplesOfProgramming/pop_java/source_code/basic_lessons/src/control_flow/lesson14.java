b0bf741c80eda8cf5fdae968b56da0e6582d310c
hanzawnyein <hanzawnyineonline@gmail.com>
2024-11-16 11:43:55 +0630

    [lesson] do while loop

---
+++ Pop-PrinciplesOfProgramming/pop_java/source_code/basic_lessons/src/control_flow/lesson14.java
@@ -0,0 +1,12 @@
+package control_flow;
+
+public class lesson14 {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while (i<101);
+    }
+}
