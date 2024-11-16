4e9aa22ee0bec73207c5191a6c494e0b38aa678f
hanzawnyein <hanzawnyineonline@gmail.com>
2024-11-16 11:18:14 +0630

    [Exercise] prime number

--- Pop-PrinciplesOfProgramming/pop_java/source_code/basic_lessons/src/control_flow/Exercise3.java
+++ Pop-PrinciplesOfProgramming/pop_java/source_code/basic_lessons/src/control_flow/Exercise3.java
@@ -3,7 +3,7 @@ package control_flow;
 public class Exercise3 {
     public static void main(String[] args) {
         boolean control = true;
-        for (int i = 3; i <= 100; i++) {
+        for (int i = 2; i <= 100; i++) {
             control = true;
             for (int j = 2; j < i; j++) {
                 if (i % j == 0) {
