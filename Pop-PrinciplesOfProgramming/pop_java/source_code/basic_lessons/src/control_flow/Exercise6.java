115bb89a6899db69fe1cffa771cfd67ce16005cb
hanzawnyein <hanzawnyineonline@gmail.com>
2024-11-16 11:20:23 +0630

    [Exercise] check prime number or not with input

---
+++ Pop-PrinciplesOfProgramming/pop_java/source_code/basic_lessons/src/control_flow/Exercise6.java
@@ -0,0 +1,36 @@
+package control_flow;
+
+import java.util.Scanner;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt user input
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is prime
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false; // Numbers <= 1 are not prime
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false; // Divisible by another number
+                    break;
+                }
+            }
+        }
+
+        // Output the result
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+}
